6012660c4f37fba828c4449f8e28be5d894d40dc
Dattatri <dattatribachare9764@gmail.com>
2024-11-26 11:25:58 +0530

    In models.py added get_total_price

--- cart/models.py
+++ cart/models.py
@@ -9,7 +9,6 @@ class CartItem(models.Model):
     user = models.ForeignKey(User, on_delete = models.CASCADE)
     date_added = models.DateTimeField(auto_now_add = True)
     
-    
     def __str__(self):
         return f'{self.quantity} - {self.product.name}'
     
